c2974792e70de3f7452eca754c42cb193bc483c8
Anuoluwa <hernuincredible@gmail.com>
2023-08-29 13:32:55 +0100

    example

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Resource from "./pages/Resource";
+import Dream from "./components/dream/Dream";
+import Jobs from "./pages/Jobs";
+
+const App = () => {
+  return (
+    <Router>
+      <Header />
+      <Resource />
+      {/* <Jobs /> */}
+      <Footer />
+    </Router>
+  );
+};
+
+export default App;
